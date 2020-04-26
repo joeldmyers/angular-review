d967f520754d73a4d0823b22f6a9552984e38919
Joel <joeldmyers@gmail.com>
2020-04-25 21:12:07 -0400

    proper import for Material components into our app

--- angular-core-workshop/apps/angular-core-workshop/src/app/app.module.ts
+++ angular-core-workshop/apps/angular-core-workshop/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular-core-workshop/material';
 
 @NgModule({
   declarations: [AppComponent],
