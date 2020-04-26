af2abbecf123a86df2a17324868e0873bc75ea01
Joel <joeldmyers@gmail.com>
2020-04-26 12:34:50 -0400

    customers component update

--- angular-core-workshop/apps/angular-core-workshop/src/app/customers/customers.module.ts
+++ angular-core-workshop/apps/angular-core-workshop/src/app/customers/customers.module.ts
@@ -4,12 +4,9 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
 
-
 @NgModule({
   declarations: [CustomersComponent],
-  imports: [
-    CommonModule,
-    CustomersRoutingModule
-  ]
+  imports: [CommonModule, CustomersRoutingModule],
+  exports: [CustomersComponent]
 })
-export class CustomersModule { }
+export class CustomersModule {}
